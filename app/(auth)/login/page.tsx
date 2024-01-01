6ce11caf7c44001294b255c9e5b38294a74729e7
rankincodes <314112@protonmail.com>
2023-12-31 20:50:42 -0500

    better login form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { signIn, signUp } from "@/lib/actions/auth"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -31,54 +33,109 @@ export default function Login({
   }
 
   const handleSignUp = async (formData: z.infer<typeof LoginFormSchema>) => {
+    console.log('signup')
     return signUp(formData)
   }
 
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Form {...form}>
-        <form
-          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
-          onSubmit={form.handleSubmit(handleSignIn)}
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="you@example.com" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Password</FormLabel>
-                <Input placeholder="********" type="password" {...field} />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button formAction={() => form.handleSubmit(handleSignIn)}>
-            Sign In
-          </Button>
-          <Button formAction={() => form.handleSubmit(handleSignUp)} variant="outline">
-            Sign Up
-          </Button>
-          {searchParams?.message && (
-            <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-              {searchParams.message}
-            </p>
-          )}
-        </form>
-      </Form>
+    <div className="flex w-full justify-center p-4">
+      <Tabs defaultValue="signin" className="w-[400px]">
+        <TabsList className="grid grid-cols-2 w-full">
+          <TabsTrigger value="signin">Sign In</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signin">
+          <Card>
+            <CardContent>
+              <Form {...form}>
+                <form
+                  className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+                  onSubmit={form.handleSubmit(handleSignIn)}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="you@example.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <Input placeholder="********" type="password" {...field} />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button >
+                    Sign In
+                  </Button>
+                  {searchParams?.message && (
+                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                      {searchParams.message}
+                    </p>
+                  )}
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="signup">
+          <Card>
+            <CardContent>
+              <Form {...form}>
+                <form
+                  className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+                  onSubmit={form.handleSubmit(handleSignUp)}
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="you@example.com" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <Input placeholder="********" type="password" {...field} />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button >
+                    Sign Up
+                  </Button>
+                  {searchParams?.message && (
+                    <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                      {searchParams.message}
+                    </p>
+                  )}
+                </form>
+              </Form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs >
     </div>
   )
 }
