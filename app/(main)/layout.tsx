e7f10fc9710b7758b80621c80e5e33fae94cf55b
rankincodes <314112@protonmail.com>
2023-12-31 19:35:15 -0500

    Import fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,4 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
-import { Toaster } from '@/components/ui/toaster'
-import { cookies } from 'next/dist/client/components/headers'
-import { redirect } from 'next/navigation'
-import { IconSettings } from '@tabler/icons-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { createClient } from '@/lib/utils/supabase/client'
 import { NavBarDropdown } from '@/components/ui/nav-dropdown'
 
 const inter = Inter({ subsets: ['latin'] })
