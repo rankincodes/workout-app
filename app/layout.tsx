e6c3e60156686c36a03af6a1e347d346298b82c7
rankincodes <314112@protonmail.com>
2024-01-11 10:55:03 -0500

    add page analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/toaster'
-import { cookies } from 'next/dist/client/components/headers'
-import { redirect } from 'next/navigation'
-import { IconSettings } from '@tabler/icons-react'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
-import { createClient } from '@/lib/utils/supabase/client'
-import { NavBarDropdown } from '@/components/ui/nav-dropdown'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -28,6 +23,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-dvh`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </html>
   )
